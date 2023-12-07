457a2859b10cd8c24b47bebe3d84062f37bea389
BrookDev <brooklynbaylisdev@gmail.com>
2023-12-07 21:43:40 +0000

    added requirements.txt and generate_docs_json.py

---
+++ generate_docs_json.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+
+def get_github_repo_contents(username, repo_name, path="", branch="main"):
+    url = f"https://api.github.com/repos/{username}/{repo_name}/contents/{path}?ref={branch}"
+    response = requests.get(url)
+    return response.json()
+
+
+def create_contents_json(username, repo_name, branch="main"):
+    contents = get_github_repo_contents(username, repo_name, branch=branch)
+    tree = build_tree(contents)
+    
+    with open("contents.json", "w") as json_file:
+        json.dump(tree, json_file, indent=2)
+
+
+def build_tree(contents):
+    tree = []
+
+    for item in contents:
+        if item["type"] == "dir":
+            subtree = get_github_repo_contents(username, repo_name, path=item["path"])
+            tree.append({
+                "name": item["name"],
+                "type": "dir",
+                "children": build_tree(subtree)
+            })
+        else:
+            tree.append({
+                "name": item["name"],
+                "type": "file"
+            })
+
+    return tree
+
+
+username = "Brooklyn-Dev"
+repo_name = "PxPaint-Docs"
+
+create_contents_json(username, repo_name)
